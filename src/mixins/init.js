957deaabc91c0832530612ff7391340d157db7ee
shenyangyang <shenyangyang@daling.com>
2018-06-10 16:21:35 +0800

    add

--- src/mixins/init.js
+++ src/mixins/init.js
@@ -3,62 +3,73 @@ import http from '@/util/request'
 import Config from '@/config/index'
 
 export default class initMixin extends wepy.mixin {
+  async saveOrUpdateUser(options, result) {
+    let {openid, userExist, userinfo} = result
+    let userIf = userinfo || {}
+    let sets = await wepy.getSetting()
+    if (sets.authSetting['scope.userInfo']) {
+      let users = await wepy.getUserInfo()
+      if (userExist === 'false') {
+        await this.saveUser(openid, users)
+      } else {
+        let nickName = encodeURIComponent(users.userInfo.nickName)
+        if (userIf.name !== nickName || userIf.headImgUrl !== users.userInfo.avatarUrl) {
+          await this.saveUser(openid, users)
+        }
+      }
+      this.ready && await this.ready(options)
+    } else {
+      this.auth && await this.auth(options)
+    }
+  }
+  async saveUser(openid, users) {
+    let userInfo = await http({
+      url: Config.api.user.save.url,
+      method: Config.api.user.save.method,
+      data: {
+        openid,
+        name: users.userInfo.nickName,
+        headImgUrl: users.userInfo.avatarUrl
+      }
+    })
+    if (userInfo && userInfo.success) {
+    } else {
+      wepy.showModal({
+        content: '用户信息读取失败，请重新打开',
+        showCancel: false
+      })
+    }
+  }
   async onLoad(options) {
     wepy.showLoading({
       title: '正在加载...',
       mask: true
     })
     let openid = wepy.getStorageSync('openid')
-    if (!openid) {
       // 重新登录
-      let sign = await wepy.login()
-      if (sign.code) {
-        let data = await http({
-          url: `${Config.api.login.url}/${sign.code}`,
-          method: Config.api.login.method
-        })
-        if (data && data.success) {
-          let res = data.result
-          let {openid} = res
-          wepy.setStorageSync('openid', openid)
-        } else {
-          wepy.showModal({
-            content: '登录失败，请重新打开',
-            showCancel: false
-          })
-        }
+    let sign = await wepy.login()
+    if (sign.code) {
+      let data = await http({
+        url: `${Config.api.login.url}/${sign.code}`,
+        method: Config.api.login.method
+      })
+      if (data && data.success) {
+        let res = data.result
+        let {openid, userExist, userinfo} = res
+        wepy.setStorageSync('openid', openid)
+        this.saveOrUpdateUser && await this.saveOrUpdateUser(options, res)
       } else {
         wepy.showModal({
           content: '登录失败，请重新打开',
           showCancel: false
         })
       }
-    }
-    let isSave = wepy.getStorageSync('isSave')
-    if (!isSave) {
-      let sets = await wepy.getSetting()
-      if (sets.authSetting['scope.userInfo']) {
-        let user = await wepy.getUserInfo()
-        let userInfo = await http({
-          url: Config.api.user.save.url,
-          method: Config.api.user.save.method,
-          data: {
-            openid,
-            name: user.userInfo.nickName,
-            headImgUrl: user.userInfo.avatarUrl
-          }
-        })
-        if (userInfo && userInfo.success) {
-          wepy.setStorageSync('isSave', true)
-        } else {
-          wepy.showModal({
-            content: '用户信息读取失败，请重新打开',
-            showCancel: false
-          })
-        }
-      }
+    } else {
+      wepy.showModal({
+        content: '登录失败，请重新打开',
+        showCancel: false
+      })
     }
     wepy.hideLoading()
-    this.ready && await this.ready(options)
   }
 }
